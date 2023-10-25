04230060d5b82cb1b7281e3ce822c387ab74440f
pascalbzk <pascalbuzake@gmail.com>
2023-10-25 16:33:51 +0845

    Create hello.js

---
+++ js/hello.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>My First JavaScript</h2>
+
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Click me to display Date and Time.</button>
+
+<p id="demo"></p>
+
+</body>
+
+<!-- Mirrored from www.w3schools.com/js/tryit.asp?filename=tryjs_myfirst by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 14 May 2018 05:27:01 GMT -->
+</html>                                                                                                                                             
